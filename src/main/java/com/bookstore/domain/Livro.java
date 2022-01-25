12f70675c43d0a7da68363c3b32b6e9b7b63c0de
gilneto <gil2alves@gmail.com>
2022-01-24 21:47:44 -0300

    novas classes

---
+++ src/main/java/com/bookstore/domain/Livro.java
@@ -0,0 +1,79 @@
+package com.bookstore.domain;
+
+import java.util.Objects;
+
+
+public class Livro {
+    private Integer id;
+    private String titulo;
+    private String nome_autor;
+    private String texto;
+    
+    private Categoria categoria;
+
+    public Livro(Integer id, String titulo, String nome_autor, String texto, Categoria categoria) {
+        this.id = id;
+        this.titulo = titulo;
+        this.nome_autor = nome_autor;
+        this.texto = texto;
+        this.categoria = categoria;
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return this.titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getNome_autor() {
+        return this.nome_autor;
+    }
+
+    public void setNome_autor(String nome_autor) {
+        this.nome_autor = nome_autor;
+    }
+
+    public String getTexto() {
+        return this.texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public Categoria getCategoria() {
+        return this.categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Livro)) {
+            return false;
+        }
+        Livro livro = (Livro) o;
+        return Objects.equals(id, livro.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    
+}
